6e1139d78539f51cc574c2f879913534ad53cc16
Omotoyosi <tundun2017@gmail.com>
2025-09-19 12:00:10 +0100

    commit

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const toggle = document.getElementById('toggle');
 const navigation = document.querySelector('.nav-bar');
+const searchForm = document.getElementById('form')
+const searchBtn = document.querySelector('.search-btn')
+const input = document.querySelector('.search')
 
 toggle.addEventListener('click', () => {
   navigation.classList.toggle('active');
 });
+
+searchBtn.addEventListener('click', () => {
+  searchForm.classList.toggle('show')
+  input.focus()
+})
